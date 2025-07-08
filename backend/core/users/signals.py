73efe573bd08bf50c48e5d5afe1a9285c13ac829
Jose Perez <16-10882@usb.ve>
2025-07-08 10:27:51 -0400

    feat(users): añadir modelo de usuario inicial y migraciones
    - Se creó la migración inicial para el modelo **User** con campos como **username**, **email** y atributos adicionales.
    - Se añadió un nuevo campo `es_lider` al modelo **User** para indicar si el usuario es líder de proyecto.
    - Se modificó el campo `p00` para que sea único y se le añadió un texto de ayuda descriptivo.
    - Se implementó una señal que crea automáticamente los grupos de usuarios y roles al finalizar la migración.

---
+++ backend/core/users/signals.py
@@ -0,0 +1,27 @@
+from django.apps import apps
+from django.db.models.signals import post_migrate
+from django.contrib.auth.models import Group, Permission
+from django.dispatch import receiver
+from core.users.models import UserRole
+
+@receiver(post_migrate)
+def crear_grupos_roles(sender, **kwargs):
+    if sender.name != "core.users":
+        return
+    
+    roles = [
+        {'nombre': UserRole.ADMINISTRADOR, 'perms': []}, 
+        {'nombre': UserRole.GERENTE, 'perms': []}, 
+        {'nombre': UserRole.COORDINADOR, 'perms': []}, 
+        {'nombre': UserRole.MIEMBRO, 'perms': []}
+    ]
+    for rol in roles:
+        grupo, creado = Group.objects.get_or_create(name=rol['nombre'])
+        for perm in rol['perms']:
+            permiso = Permission.objects.get(codename=perm)
+            grupo.permissions.add(permiso)
+
+        
+        # Asignar permisos específicos si es necesario
+        # Ejemplo: permisos = Permission.objects.filter(codename__in=['add_tarea', 'change_tarea'])
+        # grupo.permissions.set(permisos)
